b3db75f1b51af0456360184214ef70632cf01861
JamesB <James.buchmann@gmail.com>
2022-08-04 14:33:18 -0500

    servers up

---
+++ controllers/view_routes.js
@@ -0,0 +1,11 @@
+//set up the first route
+
+const view_router = require('express').Router();
+
+// get route listening on localhost:3333
+
+view_router.get('/', (request, response) => {
+    
+})
+
+

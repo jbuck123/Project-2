eb9b0f2b5ab58a71da13564c23334d71cad0b8d3
Anthony LoneFight <tonylonefight@gmail.com>
2022-08-08 10:52:13 -0500

    getting James code

--- views/howler.js
+++ views/howler.js
@@ -2,8 +2,24 @@ import {Howl, Howler} from 'howler';
 const {Howl, Howler} = require('howler');
 
 
-var sound = new Howl({
-    src: ['sound.mp3']
-  });
+let timerEnd;
+startBtn.addEventListener('click', () => {
+    buttonContainer.style.display = 'none';
+    countdownContainer.style.display = 'flex',
+
+    timerEnd = Date.now() + 10000;
+});
+
+const tick = () => {
+    if (timerEnd) {
+        const ms = timerEnd - Date.now();
+        if (ms >= 0) {
+            const mins = Math.floor()
+        }
+
+    }
+}
+
+  https://www.dropbox.com/s/v7meuouoq360vx3/hidatsa_clip.wav?dl=0
+  https://www.dropbox.com/s/vdgccnl2zepun76/hidatsa_clip.mp3?dl=0
   
-  sound.play();

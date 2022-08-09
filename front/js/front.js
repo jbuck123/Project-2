381a1f52bdeef957e0d15751943cefb2007ac9ca
Brett Gillund <brettgillund2@gmail.com>
2022-08-09 11:00:45 -0500

    adding functionality to front end api and updating index.hbs

--- front/js/front.js
+++ front/js/front.js
@@ -1,47 +1,44 @@
 const url = 'https://www.googleapis.com/books/v1/volumes?q=search-terms&key=your-API-key';
 const api_key = 'AIzaSyBSAEJ0lBONW4JVVCjjFL3QRJLm96cAQFM';
 const client_id = 'your client secret';
+// var searchBtn = document.querySelector('#search-btn');
+
+// searchBtn.addEventListener('click', function searchByTitle() {
+//   var titleSearch = document.getElementById('title-search').value;
+//   console.log(titleSearch);
+//   fetch(`https://www.googleapis.com/books/v1/volumes?q=${titleSearch}&key=${api_key}`)
+//   // fetch(`https://www.googleapis.com/books/v1/volumes?q=dune&key=${api_key}`)
+//   .then(res => res.json())
+//   .then(data => {
+//       console.log(data);
+//   });
+// })
+
+
 var searchBtn = document.querySelector('#search-btn');
+var searchResults = document.querySelector('#searchResults');
 
 searchBtn.addEventListener('click', function searchByTitle() {
   var titleSearch = document.getElementById('title-search').value;
   console.log(titleSearch);
   fetch(`https://www.googleapis.com/books/v1/volumes?q=${titleSearch}&key=${api_key}`)
-  // fetch(`https://www.googleapis.com/books/v1/volumes?q=dune&key=${api_key}`)
   .then(res => res.json())
   .then(data => {
-      console.log(data);
+      console.log(data.items);
+      for(var i = 0; i < data.items.length; i++) {
+        text = data.items[i].volumeInfo.title;
+        thumbnailImg = data.items[i].volumeInfo.imageLinks.thumbnail;
+        bookDescription = data.items[i].volumeInfo.description;
+        console.log(thumbnailImg);
+        searchResults.insertAdjacentHTML('beforeend', `
+        <div>
+          <p>${text}</p>
+          <img src="${thumbnailImg}">
+          <p>${bookDescription}</p>
+          <button>ADD TO FAVORITES</button>
+        </div>
+        `);
+
+      }
   });
 })
-
-
-// function updateSigninStatus(is_signed_in) {
-//   console.log(is_signed_in);
-//   if (is_signed_in) {
-//     // console.log(gapi.client.books.mylibrary);
-//     gapi.client.books.mylibrary.bookshelves.list().then(books => {
-//       console.log(books);
-//     })
-//   }
-// }
-
-// function initClient() {
-//   gapi.client.init({
-//     'apiKey': api_key,
-//     'clientId': client_id,
-//     'scope': 'https://www.googleapis.com/auth/books',
-//     'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/books/v1/rest']
-//   }).then(function () {
-//     const GoogleAuth = gapi.auth2.getAuthInstance();
-
-//     // Listen for sign-in state changes.
-//     GoogleAuth.isSignedIn.listen(updateSigninStatus);
-//     updateSigninStatus(GoogleAuth.isSignedIn.get());
-
-//     // GoogleAuth.signIn();
-//     gapi.auth2.getAuthInstance().signOut();
-//   });
-// }
-
-// gapi.load('client:auth2', initClient);
-

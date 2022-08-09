eed2b715e450e758db9b7ee61480bd6edf5ee153
Anthony LoneFight <tonylonefight@gmail.com>
2022-08-09 15:08:14 -0500

    new database code

--- config/db_connection.js
+++ config/db_connection.js
@@ -5,7 +5,7 @@ const { Sequelize } = require('sequelize');
 const connection = new Sequelize(
     'login_db',
     'root',
-    '',
+    'pass',
     {
         host: 'localhost',
         dialect: 'mysql',
--- views/howler.js
+++ views/howler.js
@@ -1,25 +1,31 @@
-import {Howl, Howler} from 'howler';
-const {Howl, Howler} = require('howler');
+// import {Howl, Howler} from 'howler';
+// const {Howl, Howler} = require('howler');
 
+var sfx = {
+    push: new Howl({
+        src: [
 
-let timerEnd;
-startBtn.addEventListener('click', () => {
-    buttonContainer.style.display = 'none';
-    countdownContainer.style.display = 'flex',
-
-    timerEnd = Date.now() + 10000;
-});
-
-const tick = () => {
-    if (timerEnd) {
-        const ms = timerEnd - Date.now();
-        if (ms >= 0) {
-            const mins = Math.floor()
-        }
-
-    }
+        ]
+    })
 }
 
-  https://www.dropbox.com/s/v7meuouoq360vx3/hidatsa_clip.wav?dl=0
-  https://www.dropbox.com/s/vdgccnl2zepun76/hidatsa_clip.mp3?dl=0
-  
+var music = { 
+    overworld: new Howler({
+      src: ['../howler_page/hidatsa_clip.mp3'],
+        html5: true
+    })
+};
+ 
+
+(function() {
+    document.querySelector(".play-music").addEventListener("click", ()=> {
+        console.log("I was clicked")
+        if (!music.overworld.playing()) {
+        music.overworld.play();
+        }
+    })
+    document.querySelector(".stop-music").addEventListener("click", ()=> {
+        music.overworld.pause();
+    })
+
+})();
